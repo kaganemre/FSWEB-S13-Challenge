0b226162990573bbc85823338632460c8df9e0cf
Emre Kağan Erde <emrekaganerde@outlook.com>
2025-03-07 21:23:03 +0300

    Complete all tasks

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,84 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Employee {
+
+    private int id;
+    private String fullName;
+    private String email;
+    private String password;
+    private String[] healthPlans;
+
+    public Employee(int id, String fullName, String email, String password, String[] healthplans) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.healthPlans = healthplans;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String[] getHealthPlans() {
+        return healthPlans;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setHealthplans(String[] healthplans) {
+        this.healthPlans = healthplans;
+    }
+
+    public void addHealthPlan(int index, String name) {
+        if(index >= 0 && index < healthPlans.length) {
+            if(healthPlans[index] == null) {
+                healthPlans[index] = name;
+            } else {
+                System.out.println("Index de isim var");
+            }
+        }
+        else {
+            System.out.println("Index mevcut değil");
+        }
+   }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", healthPlans=" + Arrays.toString(healthPlans) +
+                '}';
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import org.example.enums.Plan;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        checkObjects();
     }
+
+    private static void checkObjects() {
+        Healthplan healthplan =  new Healthplan(1, "B Sigorta", Plan.BASIC);
+        System.out.println(healthplan);
+
+        String[] healthplans = new String[2];
+        healthplans[0] = healthplan.getName();
+        Employee employee = new Employee(1, "Jane Doe", "jane@info.com", "123", healthplans);
+        System.out.println(employee);
+
+        String[] employees = new String[2];
+        employees[0] = employee.getFullName();
+        Company company = new Company(1, "ESP Guitars", -1, employees);
+        System.out.println(company);
+    }
+
 }
